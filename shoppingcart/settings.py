fa5f0ebe8925b6456d0db68285257ef9d4a7c4a0
Amit Vishwakrma <vishwmit1234@gmail.com>
2025-09-30 15:49:19 +0530

    exit

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -137,3 +137,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
+
+
+#SMPT CONFIGRATIONS
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'digitamit048@gmail.com'
+EMAIL_HOST_PASSWORD = 'agvsnxpxauxftkwa'
+EMAIL_USE_TLS = True
